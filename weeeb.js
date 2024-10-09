30f6001ce62196acdfcd95328a8d6dacba915d29
artem01123 <artem.kolenchenko011@gmail.com>
2024-10-09 19:09:37 +0300

    яспагмммммммммммиол

--- weeeb.js
+++ weeeb.js
@@ -1,47 +1,36 @@
-let menu = document.querySelector(".fixed-menu")
 
-menu.addEventListener("click", function(event){
+let menu = document.querySelector('.fixed-menu')
+
+menu.addEventListener('click', function (event) {
     let clickTarget = event.target
-    let activeBtn = document.querySelector(".active")
+    let activeBtn = document.querySelector('.active')
 
-    if (clickTarget.classList.conteins("nav-links")){
-        clickTarget.classList.add("active");
-        clickTarget.classList.remove("active");
+    if (clickTarget.classList.contains('nav-link')) {
+        clickTarget.classList.add('active')
+        activeBtn.classList.remove('active')
     }
-});
+})
 
+let classLink = '.main-link'
+window.onscroll = function () {
+    let h = document.documentElement.clientHeight
 
-
-let classLink = '.main-link';
-window.onscroll = function() {
-    let h = document.documentElement.clientHeight;
-
-    if (window.scrollY >= h*4) {
-        classLinklassLink = '.comments-link'
-    }
-
-    else if (window.scrollY >= h*3) {
-        classLinklassLink = '.works-link'
-    }
-
-    else if (window.scrollY >= h*2) {
-        classLinklassLink = '.skills-link'
+    if (window.scrollY >= h * 4) {
+        classLink = '.comments-link'
+    } else if (window.scrollY >= h * 3) {
+        classLink = '.works-link'
+    } else if (window.scrollY >= h * 2) {
+        classLink = '.skills-link'
+    } else if (window.scrollY >= h) {
+        classLink = '.about-link'
+    } else {
+        classLink = '.main-link'
     }
 
-    else if (window.scrollY >= h) {
-        classLinklassLink = '.about-link'
+    let activeBtn = document.querySelector('.active')
+    let newActiveBtn = document.querySelector(classLink)
+    if (!newActiveBtn.classList.contains('active')) {
+        newActiveBtn.classList.add('active')
+        activeBtn.classList.remove('active')
     }
-
-    else  {
-        classLinklassLink = '.main-link'
-    }
-
-let activeBtn = document.querySelector(".active")
-let newActiveBtn = document.querySelector(classLink)
-if (!newActiveBth.classList.contains('active')){
-    newActiveBth.classList.add('active');
-    activeBth.classList.remove('active')
-    }
-
-};
-
+}
